97f6bdc48ceb7a025352b841f57df8bf83e250bb
Cyrille59Delplanque <cyrille.delplanque@labom2iformation.fr>
2025-01-15 09:22:36 +0100

    first commit

---
+++ features/test_exo.py
@@ -0,0 +1,18 @@
+import pytest
+import pytest_bdd
+from pytest_bdd import parsers
+from pytest_bdd import scenario,given,when,then
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
+from selenium.webdriver.common.keys import Keys
+
+@pytest.fixture
+def browser():
+    #Assurer vous que le pilote est installé et configuré correctement
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+    
